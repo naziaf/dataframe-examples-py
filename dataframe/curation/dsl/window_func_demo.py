8b4c6f4fa16c5b49fb1480b50339c1c5ef246779
naziaf <nazia.nasrat@gmail.com>
2021-07-16 22:23:47 -0700

    test

--- dataframe/curation/dsl/window_func_demo.py
+++ dataframe/curation/dsl/window_func_demo.py
@@ -45,6 +45,11 @@ if __name__ == '__main__':
         .withColumn("RollingAvg", avg("Amount").over(accNumPrev4WindowSpec))\
         .show(20, False)
 
+    financeDf \
+        .withColumn("Date", to_date(from_unixtime(unix_timestamp("Date", "MM/dd/yyyy")))) \
+        .withColumn("CumulativeSum", sum("Amount").over(Window.partitionBy('AccountNumber').orderBy('Date').rowsBetween(Window.unboundedPreceding,0))) \
+        .show(20, False)
+
     productList = [
         Product("Thin", "Cell phone", 6000),
         Product("Normal", "Tablet", 1500),
